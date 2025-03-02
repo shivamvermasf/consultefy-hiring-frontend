88b09a57897d7d2edb1d89b0deafdfd84fafccde
shivamvermasf <shivamverma.sf@gmail.com>
2025-03-02 18:07:12 +0530

    Update App.js
    Fix 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
