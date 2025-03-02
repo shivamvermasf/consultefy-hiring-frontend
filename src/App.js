efd4e7a65c9e76e88609d0c521bbbb781a8793af
shivamvermasf <shivamverma.sf@gmail.com>
2025-03-02 18:44:26 +0530

    Update App.js
    Add navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 const PrivateRoute = ({ element }) => {
   const token = localStorage.getItem("token");
@@ -11,6 +12,7 @@ const PrivateRoute = ({ element }) => {
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
